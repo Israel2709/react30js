a89e83e417df2e8358cc5d1fc34482e90cabda41
Israel Salinas <israel.salinas.m@gmail.com>
2024-02-20 19:29:47 -0600

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import { useState } from "react";
-import Card from "./components/Card";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+}
 
   return (
     <>
-      <Card />
+   }
     </>
   );
 }
